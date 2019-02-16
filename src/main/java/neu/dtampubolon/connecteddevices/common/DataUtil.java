af699e90afc2d5d7ed3f968482cc2cd9befc4bf1
Doni Tampubolon <dtampubolon31@gmail.com>
2019-02-16 02:38:57 -0500

    commit

---
+++ src/main/java/neu/dtampubolon/connecteddevices/common/DataUtil.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package neu.dtampubolon.connecteddevices.common;
+
+/**
+ * @author Doni Tampubolon
+ *
+ */
+public class DataUtil {
+
+	/**
+	 * This class converts data from and to JSON strings
+	 */
+	public DataUtil() {
+		//Constructor
+	}
+	public 
+}
