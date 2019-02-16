37c70ad359e59fe3bbb48cc78d86808ae38e1b1e
Doni Tampubolon <dtampubolon31@gmail.com>
2019-02-16 01:15:04 -0500

    TempManagementApp added

---
+++ src/main/java/neu/dtampubolon/connecteddevices/labs/module05/TempManagementApp.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package neu.dtampubolon.connecteddevices.labs.module05;
+
+/**
+ * CSYE 6530 - Connected Devices
+ * Lab module 5
+ * @author Doni Tampubolon
+ *
+ */
+public class TempManagementApp {
+
+	/**
+	 * This class simulates an app that runs the sensor adaptors, actuators, and
+	 * connect to an SMTP client connector
+	 * @param args
+	 */
+	public TempManagementApp() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
