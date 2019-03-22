82934dce721ca49795a8d464024ac53065b9c682
Doni Tampubolon <dtampubolon31@gmail.com>
2019-03-22 12:25:16 -0400

    Lab module 8. tempsensorcloud pub app

---
+++ src/main/java/neu/dtampubolon/connecteddevices/labs/module08/TempSensorCloudPublisherApp.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package neu.dtampubolon.connecteddevices.labs.module08;
+
+/**
+ * @author Doni Tampubolon
+ *
+ */
+public class TempSensorCloudPublisherApp {
+
+	/**
+	 * 
+	 */
+	public TempSensorCloudPublisherApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
